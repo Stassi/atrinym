9440045f7496457e929aeac60e328d6df66f0f1b
Andreas Stassivik <andreas@stassi.net>
2023-10-13 16:33:05 -0700

    `import/no-default-export` eslint rule disabled (upstream requirement by Next.js)

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+/*
+  eslint-disable-next-line import/no-default-export --
+  Next.js requires exported default function
+  Reference: https://nextjs.org/docs/app/api-reference/next-config-js
+*/
 export default nextConfig
