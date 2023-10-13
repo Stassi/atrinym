d5f3ae2beccb04fec06645e0f8513e33edd44958
Andreas Stassivik <andreas@stassi.net>
2023-10-13 16:39:02 -0700

    superfluous asterisks removal

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,7 +2,7 @@ import React, { type JSX, type ReactNode } from 'react'
 
 /*
   eslint-disable-next-line import/no-default-export --
-  Next.js requires *exported* default function
+  Next.js requires exported default function
   Reference: https://nextjs.org/docs/app/api-reference/file-conventions/layout
 */
 export default function RootLayout({
