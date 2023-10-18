c55192269105665fa878245dcb3c7d49a3d09a29
Andreas Stassivik <andreas@stassi.net>
2023-10-18 16:25:28 -0700

    placeholder components removal

--- packages/ui-mdx/button.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export function Button({ text }: { text: string }): JSX.Element {
-  return <button type="button">{text}</button>
-}
--- packages/ui-mdx/header.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export function Header({ text }: { text: string }): JSX.Element {
-  return <h1>{text}</h1>
-}
