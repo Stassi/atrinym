f6aa70d2671ba4b91674b40c4a770f9cf6d99170
Andreas Stassivik <andreas@stassi.net>
2023-10-20 20:00:26 -0700

    `JSX` explicit type imports in `package:ui`

--- packages/ui/button.tsx
+++ packages/ui/button.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { type JSX } from 'react'
 
 export function Button({ text }: { text: string }): JSX.Element {
   return <button type="button">{text}</button>
--- packages/ui/header.tsx
+++ packages/ui/header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { type JSX } from 'react'
 
 export function Header({ text }: { text: string }): JSX.Element {
   return <h1>{text}</h1>
