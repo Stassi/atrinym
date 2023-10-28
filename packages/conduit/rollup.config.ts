363b555a7e402dd9d606843e6b285a372aa67966
Andreas Stassivik <andreas@stassi.net>
2023-10-27 22:10:51 -0700

    `package:conduit` (empty `src`) cloned from `package:arithmetic`

---
+++ packages/conduit/rollup.config.ts
@@ -0,0 +1,19 @@
+import { type RollupOptions } from 'rollup'
+import typescript from '@rollup/plugin-typescript'
+
+const rollupConfig: RollupOptions = {
+  external: ['react/jsx-runtime'],
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.mjs',
+  },
+  plugins: [
+    // @ts-expect-error -- `@rollup/plugin-typescript` default export is callable
+    typescript({
+      exclude: ['**.test.ts', 'rollup.config.ts'],
+    }),
+  ],
+}
+
+// eslint-disable-next-line import/no-default-export -- default export required by rollup.js
+export default rollupConfig
