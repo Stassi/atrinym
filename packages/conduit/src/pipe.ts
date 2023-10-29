32aca9e65a7f45aae1c099678494978c4b770136
Andreas Stassivik <andreas@stassi.net>
2023-10-28 21:56:17 -0700

    `pipe` implementation

---
+++ packages/conduit/src/pipe.ts
@@ -0,0 +1,32 @@
+/*
+  eslint-disable
+  @typescript-eslint/no-explicit-any,
+  @typescript-eslint/no-unsafe-return,
+  -- `any` required for complex type implementation(s)
+*/
+
+type UnaryFn = (...args: any[]) => any
+
+type FirstElement<T extends any[]> = T extends [infer U, ...any[]] ? U : never
+type LastElement<T extends any[]> = T extends [...any[], infer U] ? U : never
+
+// @ts-expect-error -- `unknown` type inferred
+type FirstElementParameter<T> = Parameters<FirstElement<T>>[0]
+// @ts-expect-error -- `unknown` type inferred
+type LastElementReturns<T> = ReturnType<LastElement<T>>
+
+export function nAryPipe<Fn extends UnaryFn, Fns extends Fn[]>(
+  ...fns: Fns
+): (initial: FirstElementParameter<Fns>) => LastElementReturns<Fns> {
+  return (initial: FirstElementParameter<Fns>): LastElementReturns<Fns> => {
+    return fns.reduce((accumulated: any, fn: Fn) => fn(accumulated), initial)
+  }
+}
+
+export function unaryPipe<Fn extends UnaryFn, Fns extends Fn[]>(
+  fns: [...Fns],
+): (initial: FirstElementParameter<Fns>) => LastElementReturns<Fns> {
+  return (initial: FirstElementParameter<Fns>): LastElementReturns<Fns> => {
+    return fns.reduce((accumulated: any, fn: Fn) => fn(accumulated), initial)
+  }
+}
