e127d2051a809718c6e71423d27ac5f539e4c2e8
Andreas Stassivik <andreas@stassi.net>
2023-11-05 17:41:57 -0800

    `negate` integrated within `successors` (functional consistency)

--- packages/arithmetic/src/successors.ts
+++ packages/arithmetic/src/successors.ts
@@ -1,5 +1,6 @@
 import { addBy } from './add-by.js'
+import { negate } from './negate.js'
 
 const ONE = 1
-export const decrement = addBy(-ONE)
+export const decrement = addBy(negate(ONE))
 export const increment = addBy(ONE)
