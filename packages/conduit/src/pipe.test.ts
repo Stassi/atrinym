9b919c544a2e5237eeaf7062420373bc3b0f0dfc
Andreas Stassivik <andreas@stassi.net>
2023-11-05 17:01:21 -0800

    dealiased `decrement` (`dec`) & `increment` (`inc`) for all tests in `package:conduit`

--- packages/conduit/src/pipe.test.ts
+++ packages/conduit/src/pipe.test.ts
@@ -1,12 +1,12 @@
 import { describe, expect, it } from '@jest/globals'
-import { dec as decrement, inc as increment } from 'ramda-typed'
+import { dec, inc } from 'ramda-typed'
 import { unaryPipe, variadicPipe } from './pipe.js'
 
 type NumberCallback = (n: number) => number
 
 describe('unaryPipe(...)', (): void => {
   describe('addOne(...)', (): void => {
-    const addOne: NumberCallback = unaryPipe([increment, decrement, increment])
+    const addOne: NumberCallback = unaryPipe([inc, dec, inc])
 
     it('should return one plus the given number', (): void => {
       expect(addOne(0)).toStrictEqual(1)
@@ -14,11 +14,7 @@ describe('unaryPipe(...)', (): void => {
   })
 
   describe('addThree(...)', (): void => {
-    const addThree: NumberCallback = unaryPipe([
-      increment,
-      increment,
-      increment,
-    ])
+    const addThree: NumberCallback = unaryPipe([inc, inc, inc])
 
     it('should return three plus the given number', (): void => {
       expect(addThree(0)).toStrictEqual(3)
@@ -28,7 +24,7 @@ describe('unaryPipe(...)', (): void => {
 
 describe('variadicPipe(...)', (): void => {
   describe('addOne(...)', (): void => {
-    const addOne: NumberCallback = variadicPipe(increment, decrement, increment)
+    const addOne: NumberCallback = variadicPipe(inc, dec, inc)
 
     it('should return one plus the given number', (): void => {
       expect(addOne(0)).toStrictEqual(1)
@@ -36,11 +32,7 @@ describe('variadicPipe(...)', (): void => {
   })
 
   describe('addThree(...)', (): void => {
-    const addThree: NumberCallback = variadicPipe(
-      increment,
-      increment,
-      increment,
-    )
+    const addThree: NumberCallback = variadicPipe(inc, inc, inc)
 
     it('should return three plus the given number', (): void => {
       expect(addThree(0)).toStrictEqual(3)
--- packages/conduit/src/ramda/pipe.test.ts
+++ packages/conduit/src/ramda/pipe.test.ts
@@ -1,12 +1,12 @@
 import { describe, expect, it } from '@jest/globals'
-import { dec as decrement, inc as increment } from 'ramda-typed'
+import { dec, inc } from 'ramda-typed'
 import { pipe as ramdaPipe } from './pipe.js'
 
 type NumberCallback = (n: number) => number
 
 describe('ramda pipe(...)', (): void => {
   describe('addOne(...)', (): void => {
-    const addOne: NumberCallback = ramdaPipe(increment, decrement, increment)
+    const addOne: NumberCallback = ramdaPipe(inc, dec, inc)
 
     it('should return one plus the given number', (): void => {
       expect(addOne(0)).toStrictEqual(1)
@@ -14,7 +14,7 @@ describe('ramda pipe(...)', (): void => {
   })
 
   describe('addThree(...)', (): void => {
-    const addThree: NumberCallback = ramdaPipe(increment, increment, increment)
+    const addThree: NumberCallback = ramdaPipe(inc, inc, inc)
 
     it('should return three plus the given number', (): void => {
       expect(addThree(0)).toStrictEqual(3)
