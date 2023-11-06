f7cbe66690471e20ca347d4066e939d263d981f2
Andreas Stassivik <andreas@stassi.net>
2023-11-05 23:53:03 -0800

    `subtract` implements `add` & `negate` in `package:arithmetic`

--- packages/arithmetic/src/subtract.ts
+++ packages/arithmetic/src/subtract.ts
@@ -1,3 +1,6 @@
+import { add } from './add.js'
+import { negate } from './negate.js'
+
 export function subtract(minuend: number, subtrahend: number): number {
-  return minuend - subtrahend
+  return add(minuend, negate(subtrahend))
 }
