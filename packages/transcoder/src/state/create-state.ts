9e60a51bd55d5901deb0ff4000994a995e1e3f11
Andreas Stassivik <andreas@stassi.net>
2023-11-09 13:31:40 -0800

    idiomatic `let` declaration replaces parameter reassignment, resolving `eslint-disable-next-line/no-param-reassign` exception

--- packages/transcoder/src/state/create-state.ts
+++ packages/transcoder/src/state/create-state.ts
@@ -1,13 +1,13 @@
-export function createState<T>(state: T): {
+export function createState<T>(initial: T): {
   get: () => T
   set: (x: T) => void
 } {
+  let state: T = initial
   return {
     get(): T {
       return state
     },
     set(x: T): void {
-      // eslint-disable-next-line no-param-reassign -- featured mechanic
       state = x
     },
   }
