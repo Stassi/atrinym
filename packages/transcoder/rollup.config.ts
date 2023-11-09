9bf3e0f5278ff29ffd5656440ce81851856a1acd
Andreas Stassivik <andreas@stassi.net>
2023-11-09 00:46:35 -0800

    removed erroneous ramda subpath bundle from rollup config in `package:transcoder`

--- packages/transcoder/rollup.config.ts
+++ packages/transcoder/rollup.config.ts
@@ -1,5 +1,4 @@
 import { type Plugin, type RollupOptions } from 'rollup'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
 import typescriptErroneouslyTyped, {
   type RollupTypescriptOptions,
 } from '@rollup/plugin-typescript'
@@ -8,8 +7,6 @@ type RollupPluginTypeScript = (options?: RollupTypescriptOptions) => Plugin
 
 const typescript =
     typescriptErroneouslyTyped as unknown as RollupPluginTypeScript,
-  ramdaDirectory = 'src/ramda/**.ts',
-  testFiles = '**.test.ts',
   rollupConfig: RollupOptions[] = [
     {
       input: 'src/index.ts',
@@ -18,20 +15,7 @@ const typescript =
       },
       plugins: [
         typescript({
-          exclude: [ramdaDirectory, testFiles, 'rollup.config.ts'],
-        }),
-      ],
-    },
-    {
-      input: 'src/ramda/index.ts',
-      output: {
-        file: 'dist/ramda/index.mjs',
-      },
-      plugins: [
-        nodeResolve(),
-        typescript({
-          exclude: testFiles,
-          include: ramdaDirectory,
+          exclude: ['src/ramda/**.ts', '**.test.ts', 'rollup.config.ts'],
         }),
       ],
     },
