752c86c8c6bd14e2c9a2e9ea91f5bd283f54063b
Andreas Stassivik <andreas@stassi.net>
2023-11-09 00:34:14 -0800

    `package:transcoder` created (config based on `package:arithmetic`)

---
+++ packages/transcoder/rollup.config.ts
@@ -0,0 +1,41 @@
+import { type Plugin, type RollupOptions } from 'rollup'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import typescriptErroneouslyTyped, {
+  type RollupTypescriptOptions,
+} from '@rollup/plugin-typescript'
+
+type RollupPluginTypeScript = (options?: RollupTypescriptOptions) => Plugin
+
+const typescript =
+    typescriptErroneouslyTyped as unknown as RollupPluginTypeScript,
+  ramdaDirectory = 'src/ramda/**.ts',
+  testFiles = '**.test.ts',
+  rollupConfig: RollupOptions[] = [
+    {
+      input: 'src/index.ts',
+      output: {
+        file: 'dist/index.mjs',
+      },
+      plugins: [
+        typescript({
+          exclude: [ramdaDirectory, testFiles, 'rollup.config.ts'],
+        }),
+      ],
+    },
+    {
+      input: 'src/ramda/index.ts',
+      output: {
+        file: 'dist/ramda/index.mjs',
+      },
+      plugins: [
+        nodeResolve(),
+        typescript({
+          exclude: testFiles,
+          include: ramdaDirectory,
+        }),
+      ],
+    },
+  ]
+
+// eslint-disable-next-line import/no-default-export -- default export required by rollup.js
+export default rollupConfig
