eaae99127083122ee7f60f10a8f0c25343c3fba3
Andreas Stassivik <andreas@stassi.net>
2023-12-27 03:11:47 -0800

    `binaryInversionFromBooleansInversion` function extraction

--- packages/cellular-automata/src/elementary-inversions.ts
+++ packages/cellular-automata/src/elementary-inversions.ts
@@ -6,23 +6,29 @@ export function invertColorBooleans(x: boolean[]): boolean[] {
   return reverse(x).map((y: boolean): boolean => !y)
 }
 
-export function invertColorBinary(x: string): string {
-  return booleansToBinary(invertColorBooleans(binaryToBooleans(x)))
-}
-
 // TODO: Replace with two piped swaps [(1, 4), (3, 6)] in a new general binary index swap function (package:sequences)
 export function invertLeftRightBooleans(x: boolean[]): boolean[] {
   return [x[0], x[4], x[2], x[6], x[1], x[5], x[3], x[7]] as boolean[]
-}
-
-export function invertLeftRightBinary(x: string): string {
-  return booleansToBinary(invertLeftRightBooleans(binaryToBooleans(x)))
 }
 
 export function invertColorAndLeftRightBooleans(x: boolean[]): boolean[] {
   return invertLeftRightBooleans(invertColorBooleans(x))
 }
 
-export function invertColorAndLeftRightBinary(x: string): string {
-  return booleansToBinary(invertColorAndLeftRightBooleans(binaryToBooleans(x)))
+function binaryInversionFromBooleansInversion(
+  invertBooleans: (x: boolean[]) => boolean[],
+) {
+  // noinspection JSSuspiciousNameCombination
+  return (y: string): string =>
+    booleansToBinary(invertBooleans(binaryToBooleans(y)))
 }
+
+export const invertColorBinary =
+  binaryInversionFromBooleansInversion(invertColorBooleans)
+
+export const invertLeftRightBinary = binaryInversionFromBooleansInversion(
+  invertLeftRightBooleans,
+)
+
+export const invertColorAndLeftRightBinary =
+  binaryInversionFromBooleansInversion(invertColorAndLeftRightBooleans)
