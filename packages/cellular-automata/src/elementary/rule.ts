a0ef148b5c0342c63e7185483966a53d7a448cfa
Andreas Stassivik <andreas@stassi.net>
2023-12-29 23:39:37 -0800

    `equivalences` merged into `rule`

--- packages/cellular-automata/src/elementary/rule.ts
+++ packages/cellular-automata/src/elementary/rule.ts
@@ -1,12 +1,12 @@
 import { length } from 'sequences'
-import {
-  invertColorAndLeftRightEquivalentRule,
-  invertColorEquivalentRule,
-  invertLeftRightEquivalentRule,
-} from './equivalences.js'
+import { strictEquals } from '../logic/strict-equals.js'
 import { rulesetToRule } from './ruleset-to-rule.js'
 import { ruleToBinary, ruleToBooleans } from './rule-to-ruleset.js'
-import { strictEquals } from '../logic/strict-equals.js'
+import {
+  invertColorAndLeftRightBinary,
+  invertColorBinary,
+  invertLeftRightBinary,
+} from './inversions.js'
 
 export type ElementaryRule = {
   complement: () => number
@@ -15,6 +15,10 @@ export type ElementaryRule = {
   toBinary: () => string
   toBooleans: () => boolean[]
   toDecimal: () => number
+}
+
+function equivalencesFromInversionBinary(invert: (s: string) => string) {
+  return (n: number): number => rulesetToRule(invert(ruleToBinary(n)))
 }
 
 const strictEqualsEight: (n: number) => boolean = strictEquals(8)
@@ -28,16 +32,25 @@ export function elementaryRule(x: boolean[] | number | string): ElementaryRule {
 
   return {
     complement(): number {
-      if (typeof x === 'number') return invertColorEquivalentRule(x)
-      return invertColorEquivalentRule(rulesetToRule(x))
+      const complementRule: (n: number) => number =
+        equivalencesFromInversionBinary(invertColorBinary)
+
+      if (typeof x === 'number') return complementRule(x)
+      return complementRule(rulesetToRule(x))
     },
     complementAndReflect(): number {
-      if (typeof x === 'number') return invertColorAndLeftRightEquivalentRule(x)
-      return invertColorAndLeftRightEquivalentRule(rulesetToRule(x))
+      const complementAndReflectRule: (n: number) => number =
+        equivalencesFromInversionBinary(invertColorAndLeftRightBinary)
+
+      if (typeof x === 'number') return complementAndReflectRule(x)
+      return complementAndReflectRule(rulesetToRule(x))
     },
     reflect(): number {
-      if (typeof x === 'number') return invertLeftRightEquivalentRule(x)
-      return invertLeftRightEquivalentRule(rulesetToRule(x))
+      const reflectRule: (n: number) => number =
+        equivalencesFromInversionBinary(invertLeftRightBinary)
+
+      if (typeof x === 'number') return reflectRule(x)
+      return reflectRule(rulesetToRule(x))
     },
     toBinary(): string {
       if (typeof x === 'number') return ruleToBinary(x)
