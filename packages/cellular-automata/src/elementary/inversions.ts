a4d132959567a2f5c18ab68575fdad76ee7d8b5a
Andreas Stassivik <andreas@stassi.net>
2023-12-29 23:40:13 -0800

    removed unused module exports

--- packages/cellular-automata/src/elementary/inversions.ts
+++ packages/cellular-automata/src/elementary/inversions.ts
@@ -4,16 +4,16 @@ import { reverse } from '../sequences/reverse.js'
 
 type StringCallback = (s: string) => string
 
-export function invertColorBooleans(x: boolean[]): boolean[] {
+function invertColorBooleans(x: boolean[]): boolean[] {
   return reverse(x).map((y: boolean): boolean => !y)
 }
 
 // TODO: Replace with two piped swaps [(1, 4), (3, 6)] in a new general binary index swap function (package:sequences)
-export function invertLeftRightBooleans(x: boolean[]): boolean[] {
+function invertLeftRightBooleans(x: boolean[]): boolean[] {
   return [x[0], x[4], x[2], x[6], x[1], x[5], x[3], x[7]] as boolean[]
 }
 
-export function invertColorAndLeftRightBooleans(x: boolean[]): boolean[] {
+function invertColorAndLeftRightBooleans(x: boolean[]): boolean[] {
   return invertLeftRightBooleans(invertColorBooleans(x))
 }
 
