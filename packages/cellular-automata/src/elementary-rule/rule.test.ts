5ab6e5fa21b8dbf08ec6b0e8c947cd194d076a05
Andreas Stassivik <andreas@stassi.net>
2024-01-01 18:04:24 -0800

    `validateDomain` tests replace `elementaryRule` subtests

--- packages/cellular-automata/src/elementary-rule/rule.test.ts
+++ packages/cellular-automata/src/elementary-rule/rule.test.ts
@@ -101,33 +101,4 @@ describe('elementaryRule(...)', (): void => {
       },
     )
   })
-
-  describe.each([-500, -1, 256, 500])(
-    'rule: %p (invalid octet range)',
-    (actual: number): void => {
-      it('should throw a RangeError', (): void => {
-        expect(() => elementaryRule(actual)).toThrow(
-          'Decimal octet must be in range: [0, 256)',
-        )
-      })
-    },
-  )
-
-  describe.each([
-    '',
-    '0',
-    '111101111',
-    [],
-    [false],
-    [false, false, false, false, true, false, false, false, false],
-  ])(
-    'ruleset: %p (invalid octet length)',
-    (actual: boolean[] | string): void => {
-      it('should throw a RangeError', (): void => {
-        expect(() => elementaryRule(actual)).toThrow(
-          'Octet length must equal 8',
-        )
-      })
-    },
-  )
 })
