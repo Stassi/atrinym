e67a5e2bc7208fd34ce23fdf9a2b55add6c3a8ec
Andreas Stassivik <andreas@stassi.net>
2024-01-01 16:56:32 -0800

    `reflect` implements `swap`

--- packages/cellular-automata/src/elementary-rule/equivalences.ts
+++ packages/cellular-automata/src/elementary-rule/equivalences.ts
@@ -1,5 +1,5 @@
 // TODO: Prefer native utility invariants
-import { applySpec, map, pipe, prop } from 'ramda-typed'
+import { applySpec, map, pipe, prop, swap } from 'ramda-typed'
 import { not } from '../logic/not.js'
 import { reverse } from '../sequences/reverse.js'
 import {
@@ -17,10 +17,7 @@ function complement(x: boolean[]): boolean[] {
   return reverse(x).map(not)
 }
 
-// TODO: Replace with two piped swaps [(1, 4), (3, 6)] in a new general binary index swap function (package:sequences)
-function reflect(x: boolean[]): boolean[] {
-  return [x[0], x[4], x[2], x[6], x[1], x[5], x[3], x[7]] as boolean[]
-}
+const reflect: (x: boolean[]) => boolean[] = pipe(swap(1, 4), swap(3, 6))
 
 const equivalences: (x: boolean[]) => ElementaryRuleEquivalences = pipe(
   // @ts-expect-error -- valid type
