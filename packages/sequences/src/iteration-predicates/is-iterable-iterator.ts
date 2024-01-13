2e21352543d52955d3619f6d787010b369800e2f
Andreas Stassivik <andreas@stassi.net>
2024-01-13 14:01:08 -0800

    `IterableNonNative` disambiguated from built-in `Iterable`

--- packages/sequences/src/iteration-predicates/is-iterable-iterator.ts
+++ packages/sequences/src/iteration-predicates/is-iterable-iterator.ts
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any -- implements official generic types */
 
-import { type Iterable, isIterable } from './is-iterable.js'
+import { type IterableNonNative, isIterable } from './is-iterable.js'
 import { isIterator } from './is-iterator.js'
 
 export type IterableIterator<
   T,
   TReturn = any,
   TNext = undefined,
-> = Iterable<T> & Iterator<T, TReturn, TNext>
+> = IterableNonNative<T> & Iterator<T, TReturn, TNext>
 
 export function isIterableIterator<T, TReturn = any, TNext = undefined>(
   x: IterableIterator<T, TReturn, TNext>,
--- packages/sequences/src/iteration-predicates/is-iterable.ts
+++ packages/sequences/src/iteration-predicates/is-iterable.ts
@@ -1,8 +1,12 @@
 import { strictEqualsFunction } from '../logic/strict-equals-function.js'
 
-export type Iterable<T> = { [Symbol.iterator]: () => Iterable<T> }
+export type IterableNonNative<T> = {
+  [Symbol.iterator]: () => IterableNonNative<T>
+}
 
-export function isIterable<T>(x: Iterable<T>): x is Iterable<T> {
+export function isIterable<T>(
+  x: IterableNonNative<T>,
+): x is IterableNonNative<T> {
   return (
     strictEqualsFunction(typeof x[Symbol.iterator]) &&
     x === x[Symbol.iterator]()
