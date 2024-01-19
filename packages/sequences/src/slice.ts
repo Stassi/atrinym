8bcb9784c1c99e1f3b8184569d15b9bf31a70705
Andreas Stassivik <andreas@stassi.net>
2024-01-19 01:03:55 -0800

    `slice` split into curried binary & ternary invariants

--- packages/sequences/src/slice.ts
+++ packages/sequences/src/slice.ts
@@ -1,10 +1,17 @@
+export type BinaryCallback<T> = (x: T) => T
+export type SliceableCallback<T> = BinaryCallback<Sliceable<T>>
 export type Sliceable<T> = T[] | string
 
-// TODO: Test
-export function slice<T>(
+// TODO: Test module
+
+export function sliceTernary<T>(
   start: number,
   end: number,
   collection: Sliceable<T>,
 ): Sliceable<T> {
   return collection.slice(start, end)
+}
+
+export function slice<T>(start: number, end: number): SliceableCallback<T> {
+  return (x: Sliceable<T>): Sliceable<T> => sliceTernary(start, end, x)
 }
