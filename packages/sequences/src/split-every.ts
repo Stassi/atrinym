e1518b8cfaeff48cb5b4ab3d2092a9f553d09671
Andreas Stassivik <andreas@stassi.net>
2024-01-19 01:09:42 -0800

    `splitEvery` while loop implements tacit `forEach`

--- packages/sequences/src/split-every.ts
+++ packages/sequences/src/split-every.ts
@@ -1,6 +1,15 @@
-import { createState, type State } from 'state'
+// TODO: Replace `R.pipe` with native `conduit:pipe`
+import { pipe } from 'ramda-typed'
+import { type State, createState } from 'state'
 import { length } from './length.js'
-import { slice, type Sliceable } from './slice.js'
+import {
+  type BinaryCallback,
+  type Sliceable,
+  type SliceableCallback,
+  slice,
+} from './slice.js'
+
+type Callback<T, U> = (x: T) => U
 
 type Sliced<T> = Sliceable<T>[]
 
@@ -8,18 +17,25 @@ export function splitEvery<T>(
   width: number,
   collection: Sliceable<T>,
 ): Sliced<T> {
-  const { get: remaining, set: setRemaining }: State<Sliceable<T>> =
+  const head: SliceableCallback<T> = slice(0, width),
+    tail: SliceableCallback<T> = slice(width, Infinity),
+    { get: remaining, set: setRemaining }: State<Sliceable<T>> =
       createState(collection),
     { get: generated, update: updateGenerated } = createState(
       [],
     ) as unknown as State<Sliced<T>>
 
   while (length(remaining()) > 0) {
-    updateGenerated(
-      (prev: Sliced<T>): Sliced<T> => [...prev, slice(0, width, remaining())],
-    )
-
-    setRemaining(slice(width, Infinity, remaining()))
+    ;[
+      pipe(
+        (x: Sliceable<T>): BinaryCallback<Sliced<T>> =>
+          (prev: Sliced<T>): Sliced<T> => [...prev, head(x)],
+        updateGenerated,
+      ),
+      pipe(tail, setRemaining),
+    ].forEach((fn: Callback<Sliceable<T>, void>): void => {
+      fn(remaining())
+    })
   }
 
   return generated()
