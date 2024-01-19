af6b39b84b852fe728d91ebecc78ea94c11ba033
Andreas Stassivik <andreas@stassi.net>
2024-01-18 16:11:27 -0800

    `splitEvery` implementation stub (incomplete)

---
+++ packages/sequences/src/split-every.ts
@@ -0,0 +1,9 @@
+export type Splittable<T> = T[] | string
+
+// TODO: Implement
+export function splitEvery<T>(
+  _width: number,
+  _collection: Splittable<T>,
+): Splittable<T>[] {
+  throw new Error('unimplemented')
+}
